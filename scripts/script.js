9388192b80e88e09d9d97f941bb9d24e4075a547
Justin Hairston <jhairst2@vols.utk.edu>
2022-09-15 21:31:20 -0400

    Add norse font and font folder and add invalid message to confirm-pass in js

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,7 @@ function matchCheck(firstPass, secondPass) {
     secondPass = document.querySelector("#confirm-pass");
 
     if(firstPass != secondPass) {
-        firstPass.setCustomValidity("Password do not match");
+        firstPass.setCustomValidity("Passwords do not match");
+        secondPass.setCustomValidity("Passwords do not match");
     }
 }
